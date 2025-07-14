54bc75c34af6850906b18e0e0794a1fa18e9590e
Arshdeep Singh Bhatia <arshdeepsbhatia11@gmail.com>
2025-07-14 20:26:12 +0530

    Simple SignUP login

---
+++ my-scheduler-ui/src/pages/login.js
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+export default function LoginPage() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(null)
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+        try {
+            const res = await axios.post('https://localhost:7177/api/Auth/login', {
+                email,
+                password,
+            })
+            alert('Logged in! JWT: ' + res.data)
+            setError(null)
+        } catch (err) {
+            setError(err.response?.data || 'Login failed')
+        }
+    }
+
+    return (
+        <div className="flex h-screen items-center justify-center bg-gray-100">
+            <form
+                onSubmit={handleLogin}
+                className="bg-white p-8 rounded shadow-md w-full max-w-md space-y-4"
+            >
+                <h2 className="text-2xl font-semibold text-center">Login</h2>
+                {error && <p className="text-red-500 text-sm">{error}</p>}
+
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full px-4 py-2 border rounded"
+                    required
+                />
+
+                <input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="w-full px-4 py-2 border rounded"
+                    required
+                />
+                <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="text-sm text-blue-500 ml-2"
+                >
+                    {showPassword ? "Hide" : "Show"}
+                </button>
+
+
+                <button
+                    type="submit"
+                    className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition"
+                >
+                    Log In
+                </button>
+                <p className="text-sm text-center">
+                    Dont have an account?{' '}
+                    <a href="/signup" className="text-blue-600 hover:underline">
+                        Sign Up
+                    </a>
+                </p>
+            </form>
+        </div>
+    )
+}
